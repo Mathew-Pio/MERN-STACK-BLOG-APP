a25c780fe6b260902ee703cb31d5e9fbde538a04
MathewPio <oseghaleokononfua@gmail.com>
2024-01-09 22:02:25 +0100

    mongodb database added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongodb = process.env.MONGODB_URL;
+mongoose.set('strictQuery', false);
+
+const connectDb = async() => {
+    try{
+        await mongoose.connect(mongodb)
+        console.log('Database is running')
+    }catch(err){
+        console.log(err)
+    }
+}
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`)
+    console.log(`Server is running on port 3000`);
+    connectDb();
 })
