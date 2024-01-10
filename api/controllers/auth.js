969231b6a4bb5156a7b73b38c6472d8ac94bd56d
MathewPio <oseghaleokononfua@gmail.com>
2024-01-10 23:30:41 +0100

    sign in api completed

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async(req, res, next) => {
     const { username, email, password } = req.body;
@@ -23,3 +24,32 @@ export const signup = async(req, res, next) => {
     }
 }
 
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'User not found'))
+        }
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid Password'))
+        }
+        const token = jwt.sign({
+            id: validUser._id
+        }, process.env.JWT_SECRET_KEY, {expiresIn:'3d'})
+        
+        const {password: pass, ...rest} = validUser._doc;
+
+        return res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
+    }catch(error){
+        next(error);
+    }
+}
