bc54e6977e756352151378e2353d4c027edaf101
MathewPio <oseghaleokononfua@gmail.com>
2024-01-12 10:54:44 +0100

    dark mode feature added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import {Button, Navbar, NavbarCollapse} from 'flowbite-react';
+import {Avatar, Button, Dropdown, Navbar, NavbarCollapse} from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import {TextInput} from 'flowbite-react'
 import {AiOutlineSearch} from 'react-icons/ai';
-import {FaMoon} from 'react-icons/fa';
+import {FaMoon, FaSun} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import { toogleTheme } from '../redux/theme/themeSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
+    const {currentUser} = useSelector(state => state.user)
+    const {theme} = useSelector((state) => state.theme);
   return (
     <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -25,12 +30,34 @@ export default function Header() {
             <AiOutlineSearch />
         </Button>
         <div className="flex gap-2 md:order-2">
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon />
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toogleTheme())}>
+                {theme === 'dark' ? <FaSun color='yellow' /> : <FaMoon />}
             </Button>
-            <Link to='/sign-in'>
-                <Button outline gradientDuoTone='purpleToBlue'>Sign In</Button>
-            </Link>
+            {currentUser ? (
+                <Dropdown arrowIcon={false} inline label={
+                    <Avatar alt='user' img={currentUser.profilePicture} rounded />
+                }>
+                    <Dropdown.Header >
+                        <span className='block text-sm '>@{currentUser.username}</span>
+                        <span className='block text-sm truncate font-medium '>{currentUser.email}</span>
+                        <Link to={'/dashboard?tab=profile'}>
+                            <Dropdown.Item>
+                                Profile
+                            </Dropdown.Item>
+                        </Link>
+                        <Dropdown.Divider />
+                        <Dropdown.Item>
+                            Sign Out
+                        </Dropdown.Item>
+                    </Dropdown.Header>
+                </Dropdown>
+            ) : (
+                <Link to='/sign-in'>
+                    <Button outline gradientDuoTone='purpleToBlue'>Sign In
+                    </Button>
+                </Link>
+            )
+            }
             <Navbar.Toggle />
         </div>
             <Navbar.Collapse>
