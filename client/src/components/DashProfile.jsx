fb2426b03e939ee7cc8964f5fb0abcdb35d0aefe
MathewPio <oseghaleokononfua@gmail.com>
2024-01-12 17:23:02 +0100

    Upload phot working a bit

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,12 +1,18 @@
 import { Button, TextInput } from "flowbite-react"
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from "../firebase"
 
 export default function DashProfile() {
     const {currentUser} = useSelector(state => state.user)
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
+    const[imageFileUploadProgess, setImageFileUploadingProgess] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
     const filePickerRef = useRef();
+    console.log(imageFileUploadProgess);
+    console.log(imageFileUploadError)
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         if(file){
@@ -18,7 +24,38 @@ export default function DashProfile() {
         if(imageFile){
             uploadImage();
         }
-    },[imageFile])
+    },[imageFile]);
+
+    const uploadImage = async() => {
+        // service firebase.storage {
+        //     match /b/{bucket}/o {
+        //       match /{allPaths=**} {
+        //         allow read;
+        //         allow write: if
+        //         request.resource.size < 2 * 1024 * 1024 &&
+        //         request.resource.contentType.matches('image/.*')
+        //       }
+        //     }
+        //   }
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + imageFile.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, imageFile);
+        uploadTask.on('state_changed', (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setImageFileUploadingProgess(progress.toFixed(0));
+        },
+        (error) => {
+            setImageFileUploadError('could not upload image(File must be less than 2MB)')
+        },
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                setImageFileUrl(downloadURL);
+            })
+        }
+        )
+    }
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
         <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
