7a8437811d523cd7a1969f791da90919387bddd0
MathewPio <oseghaleokononfua@gmail.com>
2024-01-13 10:55:33 +0100

    Complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,8 +1,10 @@
-import { Button, TextInput } from "flowbite-react"
+import { Alert, Button, TextInput } from "flowbite-react"
 import { useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from "../firebase"
+import {CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css'
 
 export default function DashProfile() {
     const {currentUser} = useSelector(state => state.user)
@@ -11,8 +13,6 @@ export default function DashProfile() {
     const[imageFileUploadProgess, setImageFileUploadingProgess] = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
     const filePickerRef = useRef();
-    console.log(imageFileUploadProgess);
-    console.log(imageFileUploadError)
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         if(file){
@@ -37,6 +37,7 @@ export default function DashProfile() {
         //       }
         //     }
         //   }
+        setImageFileUploadError(null)
         const storage = getStorage(app);
         const fileName = new Date().getTime() + imageFile.name;
         const storageRef = ref(storage, fileName);
@@ -46,7 +47,11 @@ export default function DashProfile() {
             setImageFileUploadingProgess(progress.toFixed(0));
         },
         (error) => {
-            setImageFileUploadError('could not upload image(File must be less than 2MB)')
+            setImageFileUploadError('Could not upload image (File must be less than 2MB)'
+            );
+            setImageFileUploadingProgess(null);
+            setImageFile(null);
+            setImageFileUrl(null);
         },
         () => {
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
@@ -61,9 +66,29 @@ export default function DashProfile() {
         <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
         <form className="flex flex-col gap-4">
             <input hidden type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} />
-            <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
-            <img src={imageFileUrl || currentUser.profilePicture} alt="user" className="rounded-full w-full border-8 h-full object-cover border-[lightgray]" />
+            <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
+                
+            {imageFileUploadProgess && (
+                <CircularProgressbar value={imageFileUploadProgess || 0} text={`${imageFileUploadProgess}%`}
+                strokeWidth={5} styles={{
+                    root: {
+                        width: '100%',
+                        height: '100%',
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                    },
+                    path: {
+                        stroke: `rgba(62, 152, 199, ${imageFileUploadProgess / 100})`,
+                    }
+                }} />
+            )}
+
+            <img src={imageFileUrl || currentUser.profilePicture} alt="user" className={`rounded-full w-full border-8 h-full object-cover border-[lightgray] ${imageFileUploadProgess && imageFileUploadProgess < 100 && 'opacity-60'}`} />
             </div>
+            {imageFileUploadError && <Alert color='failure'>
+                {imageFileUploadError}
+            </Alert>}
             <TextInput type="text" id='username' placeholder="username" defaultValue={currentUser.username} />
             <TextInput type="email" id='email' placeholder="email" defaultValue={currentUser.email} />
             <TextInput type="password" id='password' placeholder="password" />
