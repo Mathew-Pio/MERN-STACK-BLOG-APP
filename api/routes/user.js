95e0742bf3a123547e4aa969c6541a9bd3bfdb4a
MathewPio <oseghaleokononfua@gmail.com>
2024-01-14 23:09:49 +0100

    delete user function endpoin

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { updateUser } from '../controllers/user.js';
+import { deleteUser, updateUser } from '../controllers/user.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.put('/update/:userId', verifyToken ,updateUser);
+
+router.delete('/delete/:userId', verifyToken, deleteUser)
 
 export default router;
