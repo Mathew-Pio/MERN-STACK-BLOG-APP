baad19e30f6d375418bd86e5c753dd0ffbc2c3b8
MathewPio <oseghaleokononfua@gmail.com>
2024-01-14 02:08:21 +0100

    update user profile completed

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -14,7 +14,10 @@ export default function DashProfile() {
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const[imageFileUploadProgess, setImageFileUploadingProgess] = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [imageFileUploading, setImageFileUploading] = useState(false);
     const [formData, setFormData] = useState({});
+    const [updateUserSuccess, setUpdateUserSuccess] =useState(null)
+    const [updateUserError, setUpdateUserError] = useState(null)
     const filePickerRef = useRef();
     const dispatch = useDispatch();
     const handleImageChange = (e) => {
@@ -41,6 +44,7 @@ export default function DashProfile() {
         //       }
         //     }
         //   }
+        setImageFileUploading(true);
         setImageFileUploadError(null)
         const storage = getStorage(app);
         const fileName = new Date().getTime() + imageFile.name;
@@ -56,11 +60,13 @@ export default function DashProfile() {
             setImageFileUploadingProgess(null);
             setImageFile(null);
             setImageFileUrl(null);
+            setImageFileUploading(false);
         },
         () => {
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                 setImageFileUrl(downloadURL);
                 setFormData({ ...formData, profilePicture: downloadURL })
+                setImageFileUploading(false);
             })
         }
         )
@@ -72,7 +78,14 @@ export default function DashProfile() {
 
     const handleSubmit = async(e) => {
         e.preventDefault();
+        setUpdateUserError(null);
+        setUpdateUserSuccess(null)
         if(Object.keys(formData).length === 0){
+            setUpdateUserError("No changes made")
+            return;
+        }
+        if(imageFileUploading){
+            setUpdateUserError("Please wait for image to upload")
             return;
         }
         try{
@@ -87,13 +100,15 @@ export default function DashProfile() {
             const data = await res.json();
             if(!res.ok){
                 dispatch(updateFailure(data.message))
+                setUpdateUserError(data.message)
             }
             else{
                 dispatch(updateSuccess(data))
+                setUpdateUserSuccess("User's profile updated successfully");
             }
         }catch(error){
-            console.error("Error updating user:", error);
-            dispatch(updateFailure(error.message))
+            dispatch(updateFailure(error.message));
+            setUpdateUserError(error.message);
         }
     }
 
@@ -136,6 +151,16 @@ export default function DashProfile() {
             <span className="cursor-pointer">Delete Account</span>
             <span className="cursor-pointer">Sign Out</span>
         </div>
+        {updateUserSuccess && (
+            <Alert color="success" className="mt-5">
+                {updateUserSuccess}
+            </Alert>
+        )}
+        {updateUserError && (
+            <Alert color="failure" className="mt-5">
+                {updateUserError}
+            </Alert>
+        )}
     </div>
   )
 }
