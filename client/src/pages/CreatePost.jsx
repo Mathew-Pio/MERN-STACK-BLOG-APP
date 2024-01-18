4274967634d9af58a1ca6a742ed22e54a0b34784
MathewPio <oseghaleokononfua@gmail.com>
2024-01-18 13:24:42 +0100

    sreate post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,13 +6,15 @@ import { app } from '../firebase';
 import { useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
+    const navigate = useNavigate();
     const [file, setFile] =useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [formData, setFormData] = useState({})
-    console.log(formData);
+    const [publishError, setPublishError] = useState(null);
     const handleUploadImage = async() => {
         try{
             if(!file){
@@ -49,9 +51,33 @@ export default function CreatePost() {
         }
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            })
+            const data = await res.json();
+            if(!res.ok){
+                setPublishError(data.message);
+                return;
+            }
+            if(res.ok){
+                setPublishError(null)
+                navigate(`/post/${data.slug}`)
+            }
+        }catch(error){
+            setPublishError('Something went wrong')
+        }
+    }
+
     return <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div className="flex flex-col gap-4 sm:flex-row justify-between">
                 <TextInput type='text' placeholder='Title' required id='title' className='flex-1'
                 onChange={(e)=> setFormData({ ...formData, title: e.target.value })}
@@ -95,6 +121,9 @@ export default function CreatePost() {
                 setFormData({ ...formData, content: value })
             }} />
             <Button type='submit' gradientDuoTone='purpleToPink'>Publish</Button>
+            {
+                publishError && <Alert  className='mt-5' color='failure'>{publishError}</Alert>
+            }
         </form>
     </div>
 }
