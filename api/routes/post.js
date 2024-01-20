c5ebc348da7c0f65bb0429a62f9ea04ba57f49a9
MathewPio <oseghaleokononfua@gmail.com>
2024-01-20 15:51:56 +0100

    update functionality added

--- api/routes/post.js
+++ api/routes/post.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createPost, deletePost, getPosts } from '../controllers/post.js';
+import { createPost, deletePost, getPosts, updatePost } from '../controllers/post.js';
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.post('/create',verifyToken,createPost);
 
 router.get('/getposts', getPosts);
 
-router.delete('/deletepost/:postId/:userId', verifyToken, deletePost)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletePost);
+
+router.put('/updatepost/:postId/:userId', verifyToken, updatePost);
 
 export default router;
