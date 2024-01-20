6000e007aac1b5fcbee92748f22f3fae61f0bd24
MathewPio <oseghaleokononfua@gmail.com>
2024-01-20 12:05:16 +0100

    delete user functionality added

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,5 +1,6 @@
-import { Table } from "flowbite-react";
+import { Button, Modal, Table } from "flowbite-react";
 import { useEffect, useState } from "react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -7,6 +8,8 @@ export default function DashPosts() {
     const { currentUser } = useSelector((state) => state.user);
     const [userPosts, setUserPosts] = useState([]);
     const [showMore, setShowMore] = useState(true);
+    const [showModal, setShowModal] = useState(false);
+    const [postIdToDelete, setPostIdToDelete] = useState('');
     console.log(userPosts)
     useEffect(() => {
         const fetchPosts = async () => {
@@ -44,6 +47,25 @@ export default function DashPosts() {
         }
     }
 
+    const handleDeletePost = async () => {
+        setShowModal(false);
+        try{
+            const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if(!res.ok){
+                console.log(data.message)
+            }else{
+                setUserPosts((prev) => 
+                    prev.filter((post) => post._id !== postIdToDelete)
+                )
+            }
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+
   return <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 scrollbar-thumb-slate-500 ">
         {
             currentUser.isAdmin && userPosts.length > 0 ? (
@@ -71,8 +93,8 @@ export default function DashPosts() {
                     </Table.Head>
                     {
                         userPosts.map((post) => 
-                            <Table.Body className="divide-y">
-                                <Table.Row className="bg-white dark: border-gray-700 dark:bg-gray-800">
+                            <Table.Body key={post._id} className="divide-y">
+                                <Table.Row key={post._id} className="bg-white dark: border-gray-700 dark:bg-gray-800">
                                     <Table.Cell>{
                                         new Date(
                                         post.updatedAt).toLocaleDateString()}
@@ -89,7 +111,10 @@ export default function DashPosts() {
                                         {post.category}
                                     </Table.Cell>
                                     <Table.Cell>
-                                        <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                                        <span onClick={() => {
+                                            setShowModal(true);
+                                            setPostIdToDelete(post._id);
+                                        }} className='font-medium text-red-500 hover:underline cursor-pointer'>
                                             Delete
                                         </span>
                                     </Table.Cell>
@@ -115,5 +140,18 @@ export default function DashPosts() {
                 <p>You have no posts yet!</p>
             )
         }
+        <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md' >
+            <Modal.Header />
+            <Modal.Body>
+                <div className="text-center">
+                    <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                    <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete your post?</h3>
+                    <div className="flex justify-center gap-4">
+                        <Button color="failure" onClick={handleDeletePost}>Yes, I'm sure</Button>
+                        <Button color="gray" onClick={() => setShowModal(false)}>No, cancel</Button>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
     </div>
 }
