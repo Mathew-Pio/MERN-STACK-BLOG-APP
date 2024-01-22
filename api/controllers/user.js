d4c9ef9b7496e2a31b05f272d752e25bb464dcde
MathewPio <oseghaleokononfua@gmail.com>
2024-01-22 23:42:52 +0100

    new changes

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -44,7 +44,7 @@ export const updateUser = async(req, res, next) => {
 }
 
 export const deleteUser = async (req, res, next) => {
-    if(req.user.id !== req.params.userId){
+    if(!req.user.isAdmin && req.user.id !== req.params.userId){
         return next(errorHandler(403, "You are not allowed to delete this user"));
     }
     try{
@@ -101,8 +101,4 @@ export const getUsers = async (req, res, next) => {
         next(error);
     }
 };
-
-
-
-
 
