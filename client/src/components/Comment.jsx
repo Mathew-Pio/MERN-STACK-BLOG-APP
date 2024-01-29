2baf2457d95a3bf3dff6443d4612771c5716db5a
MathewPio <oseghaleokononfua@gmail.com>
2024-01-29 23:34:48 +0100

    new commit

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -40,7 +40,7 @@ export default function Comment({comment, onLike}) {
             </div>
             {
                 isEditing ? (
-                    <Textarea className="w-full p-2 text-gray-700 rounded-md resize-none focus:outline-none focus:bg-gray-100" rows='3' value={editedContent} onChange={(e) => setComment(e.target.value)} />
+                    <Textarea className="w-full p-2 text-gray-700 rounded-md resize-none focus:outline-none focus:bg-gray-100" rows='3' value={editedContent} onChange={(e) => setEditedContent(e.target.value)} />
                 ) : (
                 <>
                 <p className="text-gray-500 pb-2">{comment.content}</p>
