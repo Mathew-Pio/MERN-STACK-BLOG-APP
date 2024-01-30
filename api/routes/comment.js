f737727fd8957f79acc9f8f919c137abac7cdb16
MathewPio <oseghaleokononfua@gmail.com>
2024-01-30 23:26:24 +0100

    server adjusted

--- api/routes/comment.js
+++ api/routes/comment.js
@@ -10,6 +10,6 @@ router.get('/getPostComments/:postId', getPostComments );
 
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 
-router.put('/likeComment/:commentId', verifyToken, editComment)
+router.put('/editComment/:commentId', verifyToken, editComment)
 
 export default router;
